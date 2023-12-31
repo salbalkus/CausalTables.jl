15406d8b5292ee2b088180452ded63e721cd696e
Documenter.jl <documenter@juliadocs.github.io>
2023-12-31 00:57:18 +0000

    build based on 9c1212d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CausalTable.jl","page":"CausalTable.jl","title":"CausalTable.jl","text":"","category":"section"},{"location":"","page":"CausalTable.jl","title":"CausalTable.jl","text":"```@docs CausalTable CausalTable(tbl)","category":"page"}]
+}
