6817f71d87c8e42d845b2c53e3fd1a79a3551dc1
Salvador Balkus <salbalkus@gmail.com>
2023-12-30 19:41:50 -0500

    edit make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,6 @@ using Documenter, CausalTables
 
 makedocs(sitename="CausalTables.jl")
 deploydocs(
-    repo = "https://github.com/salbalkus/CausalTables.jl.git",
+    repo = "github.com/salbalkus/CausalTables.jl.git",
+    
 )
