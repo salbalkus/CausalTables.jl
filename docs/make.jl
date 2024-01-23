6cfb4e58191f3b9243a91105cdd556650298fd2a
Salvador Balkus <salbalkus@gmail.com>
2024-01-22 19:24:24 -0500

    fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,11 @@ using Documenter, CausalTables
 makedocs(
     sitename="CausalTables.jl",
     pages = [
-        "Home" => "index.md".
+        "Home" => "index.md",
         "Getting Started" => "getting_started.md",
         "Turning data into a `CausalTable`" => "formatting.md",
         "Generating Data for Statistical Experiments" => "generating_data.md",
-        "Computing Ground Truth of Causal Parameters" => "ground_truth.md"
+        "Computing Ground Truth of Causal Parameters" => "ground_truth.md",
         "User-Defined Network Summaries" => "network_summaries.md",
     ]
 )
